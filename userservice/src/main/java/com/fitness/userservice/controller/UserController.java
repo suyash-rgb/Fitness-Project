3235eae613b33b5cf8a0c655932a04f9e391302b
Suyash Baoney <suyashbaoney58@gmail.com>
2025-04-12 18:10:56 +0530

    register User and getUserProfile functionality created
    - Added the constructors and getters and setters (not using Lombok)
    - cretaed the repository and service layer
    - created RegisterRequest and UserResponse DTO
    - created endpoints /register and /{userId}

---
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.fitness.userservice.controller;
+
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.service.UserService;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId){
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid  @RequestBody RegisterRequest request){
+        return ResponseEntity.ok(userService.register(request));
+    }
+
+
+}
--- userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
+++ userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
@@ -1,51 +1,21 @@
-package com.fitness.userservice.model;
+package com.fitness.userservice.dto;
 
+import com.fitness.userservice.model.UserRole;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "users")
-public class User {
+public class UserResponse {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-
-    @Column(unique = true, nullable = false)
     private String email;
-
-    @Column(nullable = false)
     private String password;
     private String firstName;
     private String lastName;
-
-    @Enumerated(EnumType.STRING)
-    private UserRole role = UserRole.USER;
-
-    @CreationTimestamp
     private LocalDateTime createdAt;
-
-    @UpdateTimestamp
     private LocalDateTime updatedAt;
-
-    public User() {
-    }
-
-    public User(String id, String email, String password, String firstName, String lastName, UserRole role, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.role = role;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
 
     public String getId() {
         return id;
@@ -85,14 +55,6 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
     }
 
     public LocalDateTime getCreatedAt() {
