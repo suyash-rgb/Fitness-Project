56b355b964976c14c83e4875a9e330573cda7c81
Suyash Baoney <suyashbaoney58@gmail.com>
2025-04-12 15:01:40 +0530

    Initial Commit
    created the first microservice - userservice
    
    created an entity User, enum UserRole with values User and Admin
    
    database file included

---
+++ fitness_user_db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE fitness_user_db;
+
+USE fitness_user_db;
+
+SHOW TABLES;
+
+DESCRIBE users;
---
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -0,0 +1,35 @@
+package com.fitness.userservice.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
