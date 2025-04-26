21ad2ca35555e9df7c77d42a59bbcedfe32bd983
Suyash Baoney <suyashbaoney58@gmail.com>
2025-04-26 16:18:01 +0530

    adding repository, service and controller layers to aiservice
    created Repository, service and controller layers
    
    created endpoints :
    
    getActivityRecommendation, getUserRecommendation

---
+++ aiservice/src/main/java/com/fitness/aiservice/controller/RecommendationController.java
@@ -0,0 +1,32 @@
+package com.fitness.aiservice.controller;
+
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.service.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/recommendations")
+public class RecommendationController {
+
+    @Autowired
+    private RecommendationService recommendationService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(@PathVariable String userId){
+
+        return ResponseEntity.ok(recommendationService.getUserRecommendation(userId));
+    }
+
+    @GetMapping("/user/{activityId}")
+    public ResponseEntity<Recommendation> getActivityRecommendation(@PathVariable String activityId){
+
+        return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
+    }
+}
---
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -0,0 +1,24 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecommendationService {
+
+    @Autowired
+    private RecommendationRepository recommendationRepository;
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(()-> new RuntimeException("No Recommendation found for this activity:"+activityId));
+    }
+}
